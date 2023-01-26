b7ac5d2eb6df8dec78b3048e9723be10d8aa7489
parisadikshit <2018bec034@sggs.ac.in>
2023-01-26 19:24:53 +0530

    first commit

---
+++ testwithjest/test/subtract.test.js
@@ -0,0 +1,5 @@
+const subtract = require('../js/subtract')
+
+test('Subtracts 2 numbers', ()=>{
+    expect(subtract(2,1)).toBe(1)
+})
---
+++ testwithjest/test/sum.test.js
@@ -0,0 +1,5 @@
+const sum = require('../js/sum')
+
+test('Adds 2 numbers', ()=>{
+    expect(sum(1,2)).toBe(3)
+})
